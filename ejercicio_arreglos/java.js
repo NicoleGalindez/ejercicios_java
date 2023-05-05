a7976f071e1e703d8d4fb94243b77ddaf445affe
NicoleGalindez <129116184+NicoleGalindez@users.noreply.github.com>
2023-05-05 15:32:22 -0500

    destructuring

---
+++ ejercicio_arreglos/java.js
@@ -0,0 +1,8 @@
+
+// destructuring
+
+const semana =['lunes', 'martes', 'miercoles', 'jueves', 'viernes'];
+const [dia1, dia2, dia3, dia4, dia5]= semana;
+console.log(dia3);
+console.log(dia4);
+console.log(dia5);
--- ejercicio_objetos/ejercicio_tutor_menor/javas.js
+++ ejercicio_objetos/ejercicio_tutor_menor/javas.js
@@ -1,77 +1,3 @@
-/*const motero ={
-    nombre:"Cristian Samir",
-    apellido:"Narvaez",
-    telefono: 317665403,
-    detalle :function(){
-        console.log(`El apellido del motero es ${this.apellido }`)
-    }
-}
-console.log(motero)
-
-motero.detalle();*/
-
-//-------------------------------------------
-
-/*
-const libros = ['La iliada','La odisea','El Moro','El quijote','La Bbiblia'];
-const meses = new Array ('Enero', 'Febrero', 'Marzo','Abril');
-
-console.log(libros);
-console.table(libros);
-console.log(meses);
-console.table(meses);
-//---------------------
-const datos= ['ADSO', 2556678, null, true,{nombre:'Manuel ', apellido:'Camacho', sexo:'de vez en cuando'}];
-console.log(datos);
-console.table(datos); 
-console.log(datos[0],datos[1]); 
-//_________________________________
- 
-
-const aprendices =['Luis','Jhonatan','Jhon','Damiel','Brayan'];
-console.log(aprendices);
-
-for(let z=0; z<5; z++)  {
-    console.log(aprendices[z])
-}
- 
-//--------------------------------------------
-const libros = ['La iliada','La odisea','cien años de soledad','la maria'];
-console.log(libros);
-libros[10]=25000;
-libros[4]="principito";
-console.log(libros);
-console.table(libros);
-
-
-libros.push('la biblia');   //ultimo
-console.table(libros);
-
-libros.unshift('El primero de la lista');   //primero
-console.table(libros);
-
-*/
-//__-------------------------------
-
-/*
-let nombre_tutor= prompt("Nombre del nombre tutor (1)");
-console.log(nombre_tutor);
-console.table
-
-prompt= [{nombre_tutor}];
-
-*/
-
-//       const aprendiz1 = prompt("Aprendiz #1. ¿cual es tu nombre?");
-//       const datos= ['ADSO', 2556678, null, true,{nombre:'Manuel ', apellido:'Camacho', sexo:'de vez en cuando'}];
-
-
-
-
-
-//--------------------------------------
-
-
 
 //_________________________EJERCICIO/ 04/05/23______________________ 
 //alert('mensaje');
@@ -137,7 +63,11 @@ console.table(tutor);
 
 
 
-//    _________________________cbo________________________
+//    ________________________05/05/23______________________
+
+
+
+
 const nombre =[];
 
 
